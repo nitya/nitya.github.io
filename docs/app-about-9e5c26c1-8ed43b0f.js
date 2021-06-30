09cf97c2d4d66029af55ceef2020f457f3cc9b70
Nitya Narasimhan <n.narasimhan@gmail.com>
2021-06-30 14:03:59 -0400

    Testing PWA

---
+++ docs/app-about-9e5c26c1-8ed43b0f.js
@@ -0,0 +1,5 @@
+import{h as t,i as e,T as r,_ as s,n as a}from"./app-index-0b026a93.js";let n=class extends t{static get styles(){return e``}constructor(){super()}render(){return r`
+      <div>
+        <h2>About Page</h2>
+      </div>
+    `}};n=s([a("app-about")],n);export{n as AppAbout};
---
+++ docs/app-index.js
@@ -0,0 +1,1 @@
+export{A as AppIndex}from"./app-index-0b026a93.js";
